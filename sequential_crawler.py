21c7df168302f7bec4b71b0d16cc02b778d5e27c
Hari <68576993+Hari01V@users.noreply.github.com>
2021-09-06 23:57:57 +0530

    Update sequential_crawler.py

--- sequential_crawler.py
+++ sequential_crawler.py
@@ -1,72 +1,51 @@
-import requests
-import multiprocessing
 from bs4 import BeautifulSoup
-from queue import Queue, Empty
-from concurrent.futures import ThreadPoolExecutor
+import urllib.request
 from urllib.parse import urljoin, urlparse
+import nltk
+from nltk.corpus import stopwords
 
+response = urllib.request.urlopen('https://github.com/')
+root_url = "https://github.com/"
 
-class MultiThreadScraper:
-    # contents = list()
+html = response.read()
 
-    def __init__(self, base_url):
-        self.base_url = base_url
-        self.root_url = '{}://{}'.format(
-            urlparse(self.base_url).scheme,
-            urlparse(self.base_url).netloc)
-        self.pool = ThreadPoolExecutor(max_workers=1)
-        self.scrapped_pages = set([])
-        self.to_crawl = Queue()
-        self.to_crawl.put(self.base_url)
+soup = BeautifulSoup(html, "lxml")
 
-    def parse_links(self, html):
-        soup = BeautifulSoup(html, 'html.parser')
-        links = soup.find_all('a', href=True)
-        for link in links:
-            url = link['href']
-            if url.startswith('/') or url.startswith(self.root_url):
-                url = urljoin(self.root_url, url)
-                if url not in self.scrapped_pages:
-                    self.to_crawl.put(url)
+text = soup.get_text(strip='true')
+# print(text)
 
-    def scrape_info(self, html):
-        soup = BeautifulSoup(html, "html5lib")
-        text = soup.get_text(strip=True)
-        # print(text)
-        return
+links = soup.find_all('a', href=True)
+# print(links)
 
-    def post_scrape_callback(self, res):
-        result = res.result()
-        if result and result.status_code == 200:
-            self.parse_links(result.text)
-            self.scrape_info(result.text)
+outlinks = []
 
-    def scrape_page(self, url):
-        try:
-            res = requests.get(url, timeout=(3, 30))
-            return res
-        except requests.RequestException:
-            return
+for link in links:
+    url = link['href']
+    print("\nScrapping URL: {}\n".format(url))
+    if url.startswith('/') or url.startswith(root_url):
+        url = urljoin(root_url, url)
+        outlinks.insert(1, url)
 
-    def run_scraper(self):
-        while True:
-            try:
-                print("\n name of the Process:\n",
-                      multiprocessing.current_process().name)
-                target_url = self.to_crawl.get(timeout=60)
-                if target_url not in self.scrapped_pages:
-                    print("Scrapping URL: {}".format(target_url))
-                    self.scrapped_pages.add(target_url)
-                    job = self.pool.submit(self.scrape_page, target_url)
-                    job.add_done_callback(self.post_scrape_callback)
-            except Empty:
-                print("CRAWLING COMPLETED")
-                return
-            except Exception as e:
-                # print(e)
-                continue
+tokens = [t for t in text.split()]
 
+print("\nNumber Of Tokens: {}\n".format(len(tokens)))
 
-if __name__ == '__main__':
-    s = MultiThreadScraper("https://github.com/")
-    s.run_scraper()
+clean_tokens = tokens[:]
+
+sr = stopwords.words('english')
+
+for token in tokens:
+    if token in stopwords.words('english'):
+        clean_tokens.remove(token)
+
+print("Tokens after stop words removal \n", clean_tokens)
+print(len(clean_tokens))
+
+print("no of stopwords =\n", len(tokens) - len(clean_tokens))
+
+freq = nltk.FreqDist(clean_tokens)
+print("\n Frequency", freq, "\n")
+print(freq.items())
+
+for key, val in freq.items():
+    print(str(key) + ':' + str(val))
